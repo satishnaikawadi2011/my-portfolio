60cea254f25a96286408088a6c81ac008e6ba62a
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-02-04 14:46:14 +0530

    Almost completed navbar for all screens by some hacks.

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const navItems = document.querySelectorAll('.nav__items');
 const navbar = document.querySelectorAll('.navbar');
 const menuIcon = document.querySelectorAll('.nav__menu__icon');
-var x = window.matchMedia('(min-width: 768px)');
+var x = window.matchMedia('(min-width: 992px)');
 
 menuIcon[0].addEventListener('click', function(e) {
 	if (x.matches) {
 		navbar[0].style.height = '4rem';
+		navItems[0].style.display = 'flex';
 	}
 	else {
 		if (navItems[0].style.display != 'none') {
@@ -13,10 +14,25 @@ menuIcon[0].addEventListener('click', function(e) {
 			navbar[0].style.height = '4rem';
 		}
 		else {
-			navItems[0].style.display = 'block';
+			navItems[0].style.display = 'flex';
 			navbar[0].style.height = '22rem';
 		}
 	}
 });
+
+if (x.matches) {
+	window.onscroll = () => {
+		console.log('Here');
+		if (this.scrollY == 0) {
+			navbar[0].style.backgroundColor = 'transparent';
+		}
+		if (this.scrollY >= 50) {
+			navbar[0].style.backgroundColor = '#222';
+		}
+		else {
+			navbar[0].style.backgroundColor = 'transparent';
+		}
+	};
+}
 
 console.log('Script loaded');
